f0dfec190c3f5701baa2e7c9c05f47669bcb5083
Bhavesh Jain <bhaveshjain@Bhaveshs-MacBook-Pro.local>
2020-12-03 18:33:48 +0000

    hello

--- Slingshot.js
+++ Slingshot.js
@@ -6,9 +6,13 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+
+        this.sling1Image = loadImage("sprites/sling1.png");
+        this.sling2Image = loadImage("sprites/sling2.png");
+        this.sling3Image = loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -17,11 +21,17 @@ class SlingShot{
 
     display(){
         if(this.sling.bodyA){
+            
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            stroke("#47210D");
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            line(pointA.x-15, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+25, pointA.y, pointB.x+25, pointB.y);
+            image(this.sling3Image,pointA.x-18,pointA.y);
         }
+        image(this.sling1Image,200,20);
+        image(this.sling2Image,170,10);
     }
     
 }
